ee516e9f896ec70dc4660036a76704c79fbfc6e3
MAC <chuxoguh@gmail.com>
2022-01-23 02:44:52 +0100

    Assignment

---
+++ first_week_assignment.py
@@ -0,0 +1,19 @@
+my_list = ["The", "quick", "brown", "fox",
+           "jumps", "over", "the", "lazy", "dog."]
+
+list1 = " ".join(my_list)
+
+print(list1)
+
+list = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+
+list2 = list[1:4]
+
+print(list2)
+
+color_list = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+
+color = input("Enter your favourite color: ")
+
+color_list.append(color)
+print(color_list)
